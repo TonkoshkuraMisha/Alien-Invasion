319433406d805d766d1dea88d1e7ba91012cc73b
tonkoshkura.misha <tonkoshkura.misha@gmail.com>
2021-03-22 23:10:07 +0200

    Пули сбивают пришельцев!

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings():
         self.ship_speed = 1.5
         # Параметры снаряда.
         self.bullet_speed = 2.5
-        self.bullet_width = 4
+        self.bullet_width = 5
         self.bullet_height = 20
         self.bullet_color = (255, 255, 25)
         self.bullets_allowed = 15
