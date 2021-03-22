081dcaa545a8f779573a5aedd8df8bc8546c8c3e
tonkoshkura.misha <tonkoshkura.misha@gmail.com>
2021-03-22 22:16:40 +0200

    Aliens!

--- settings.py
+++ settings.py
@@ -7,10 +7,15 @@ class Settings():
         self.screen_height = 768
         self.bg_color = (0, 200, 0)
         # Настройки корабля.
-        self.ship_speed = 1.2
+        self.ship_speed = 1.5
         # Параметры снаряда.
-        self.bullet_speed = 2.2
-        self.bullet_width = 5
+        self.bullet_speed = 2.5
+        self.bullet_width = 4
         self.bullet_height = 20
         self.bullet_color = (255, 255, 25)
-        self.bullets_allowed = 5
+        self.bullets_allowed = 15
+        # Настройки пришельцев.
+        self.alien_speed = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
+        self.fleet_direction = 1
